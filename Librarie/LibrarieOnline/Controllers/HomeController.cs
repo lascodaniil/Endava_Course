6a8765c936e761c33fc7c3b201752288c218417d
lascodaniil <lascodaniil@gmail.com>
2019-03-20 19:06:08 +0200

    init

--- Librarie/LibrarieOnline/Controllers/HomeController.cs
+++ Librarie/LibrarieOnline/Controllers/HomeController.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibrarieOnline.Models;
 
 namespace LibrarieOnline.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult About()
         {
             return View();
+        }
+
+        public ActionResult Books()
+        {
+            var books = new List<Book>() {
+                new Book()
+                {
+                    ID = 1,
+                    Name = "Song of Ice and Fire",
+                    Description = "Action Fantasy",
+                    auth = new Author
+                    {
+                        NR_carti = 1,
+                        Name ="Nicolae ",
+                        LastName = "Dabija"
+                    }
+                }
+            };
+            return View(books);
         }
     }
 }
---
+++ Librarie/LibrarieOnline/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibrarieOnline.Models
+{
+    public class Author
+    {
+        public int NR_carti { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
